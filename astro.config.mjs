c5828d2649cdba71d78e64d48d040f5ced3cf1c5
vittoriaromano <vittoria.romano@student.howest.be>
2025-11-19 12:05:25 +0100

    fix path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://vitto140.github.io',
-    base: '/cck/',
+    base: '/astro-deployment/',
 });
